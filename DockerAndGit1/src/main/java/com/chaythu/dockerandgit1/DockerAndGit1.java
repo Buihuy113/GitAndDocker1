415aa6d0e5e6ab4396bb76c631ce68e79b672a36
Buihuy113 <162303539+Buihuy113@users.noreply.github.com>
2025-03-14 01:02:18 +0700

    first commit

---
+++ DockerAndGit1/src/main/java/com/chaythu/dockerandgit1/DockerAndGit1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.chaythu.dockerandgit1;
+
+/**
+ *
+ * @author duchuy
+ */
+public class DockerAndGit1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
