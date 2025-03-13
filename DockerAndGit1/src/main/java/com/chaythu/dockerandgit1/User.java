c77800131028d1a3dd9098af298124334f230a1f
Buihuy113 <162303539+Buihuy113@users.noreply.github.com>
2025-03-14 01:39:28 +0700

    sửa update data

---
+++ DockerAndGit1/src/main/java/com/chaythu/dockerandgit1/User.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.chaythu.dockerandgit1;
+
+/**
+ *
+ * @author duchuy
+ */
+public class User {
+    private String userId;
+    private String email;
+
+    public User(String userId, String email) {
+        this.userId = userId;
+        this.email = email;
+    }
+
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public void updateEmail(String newEmail) {
+    if (!newEmail.contains("@")) {
+        throw new IllegalArgumentException("Email không hợp lệ!");
+    }
+    this.email = newEmail;
+}
+
+}
